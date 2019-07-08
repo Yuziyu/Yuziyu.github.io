17a3989f434d7b6dafcbe1dfbdf133f9e823032c
masataka atsumi <matsumi@studioz.co.jp>
2019-07-08 19:31:28 +0900

    file add

--- js/zapp.js
+++ js/zapp.js
@@ -1,7 +1,25 @@
 $(function() {
-	setTimeout(
-		function() {
-			$(".top_logo").fadeIn();
-		}, 300
-	)
+
+	// ページ遷移アニメーション
+	$(document).ready(function() {
+		$(".animsition").animsition({
+			inClass: 'zoom-in-sm',
+			outClass: 'zoom-out-sm',
+			inDuration: 200,
+			outDuration: 300,
+			linkElement: '.animsition-link',
+			loading: true,
+			loadingParentElement: 'body', //animsition wrapper element
+			loadingClass: 'animsition-loading',
+			loadingInner: '', // e.g '<img src="loading.svg" />'
+			timeout: false,
+			timeoutCountdown: 5000,
+			onLoadEvent: true,
+			browser: [ 'animation-duration', '-webkit-animation-duration'],
+			overlay : false,
+			overlayClass : 'animsition-overlay-slide',
+			overlayParentElement : 'body',
+			transition: function(url){ window.location.href = url; }
+		});
+	});
 });
