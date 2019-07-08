5b815f577ae666d0d38e6763341eebcf6a7b5268
masataka atsumi <matsumi@studioz.co.jp>
2019-07-08 20:27:12 +0900

    file add

--- js/zapp.js
+++ js/zapp.js
@@ -22,4 +22,32 @@ $(function() {
 			transition: function(url){ window.location.href = url; }
 		});
 	});
+
+
+    $("#send").on("click", function() {
+		var token = "b98fe3fa8747957f4266d2490d1114f7";
+		var rid	= 155304761;
+		var mid = "ぴ";
+		$.ajax({
+			url: "https://api.chatwork.com/v2/rooms/" + rid + "/messages/" + mid,
+			headers: {
+				'X-ChatWorkToken': token
+			},
+            xhrFields: { withCredentials: true },
+			type: 'GET',
+			json: true
+		}).done(function (data, status, xhr) {
+			if (xhr.status === 200) {
+				// 正常処理
+			} else {
+				// 例外処理
+			}
+			return true;
+		}).fail(function (data, status, xhr) {
+			// エラー時の処理
+			return false;
+		}).always(function () {
+			return true;
+		});
+	});
 });
